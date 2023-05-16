9cd9909a3463f6997da3634d9d41c37dcb7c2639
lx <3282614044@qq.com>
2023-05-16 09:33:08 +0800

    第一个web项目

--- src/main/java/com/lx/community/controller/IndexController.java
+++ src/main/java/com/lx/community/controller/IndexController.java
@@ -7,12 +7,11 @@ import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name="name") String name , Model model)
+public class IndexController {
+    @GetMapping("/")
+    public String hello(Model model)
     {
-        model.addAttribute("name" , name);
-        return "hello";
+        return "index";
     }
 
 }
